a9fbc38572bab4f7238f28faa98f7e031e80dc64
Nathan Dill <littlenate7@gmail.com>
2018-11-20 17:33:23 -0600

    Created User model to hold data for logged in user

---
+++ SQLConnectionWebApp/Models/User.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SQLConnectionWebApp.Models
+{
+    public class user
+    {
+        bool loggedIn = false;
+        string username;
+
+        public string Username { get => username; set => username = value; }
+        public bool LoggedIn { get => loggedIn; set => loggedIn = value; }
+
+        public static user getUser()
+        {
+            var userSelection = (user)HttpContext.Current.Session["user"];
+            if (userSelection == null)
+            {
+                HttpContext.Current.Session.Add("user", new user());
+                userSelection = (user)HttpContext.Current.Session["user"];
+            }
+            return userSelection;
+        }
+    }
+}
