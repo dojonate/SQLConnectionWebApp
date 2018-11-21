08ce7d1ea675d1a8c72e25c67b5c1550e413da29
Nathan Dill <littlenate7@gmail.com>
2018-11-20 21:03:14 -0600

    Updated MyAccount page

--- SQLConnectionWebApp/Pages/MyAccount.aspx
+++ SQLConnectionWebApp/Pages/MyAccount.aspx
@@ -2,5 +2,45 @@
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
-    
+    <div id="loggedOut" runat="server" visible="false">
+        <asp:Label runat="server" ID="logInFirstLbl" Text="Please log in first!" ></asp:Label><br />
+        <asp:Label runat="server" ID="RedirectLbl" Text="You will be redirected to the login page in 10 seconds..."></asp:Label>
+    </div>
+    <div id="loggedIn" runat="server" visible="false">
+        <asp:Label runat="server" ID="usernameLbl" Text="Username: "></asp:Label>
+        <asp:Label runat="server" ID="usernamePrintLbl" Text=""></asp:Label><br /><br />
+        <div id="changePassword">
+            <asp:Label runat="server" ID="changePasswordLbl" Text="Change Password: " Font-Bold="true"></asp:Label><br />
+            <asp:Label runat="server" ID="oldPasswordLbl" Text="Old Password: "></asp:Label><br />
+            <asp:TextBox runat="server" ID="oldPassword" TextMode="Password"></asp:TextBox><asp:RequiredFieldValidator runat="server" 
+                ID="oldPasswordValidator" ControlToValidate="oldPassword" Text="Old password required"></asp:RequiredFieldValidator><br />
+            <asp:Label runat="server" ID="newPasswordLbl" Text="New Password (if changing): "></asp:Label><br />
+            <asp:TextBox runat="server" ID="newPassword" TextMode="Password"></asp:TextBox><asp:CustomValidator runat="server" ID="newPasswordChecker" 
+                ControlToValidate="newPassword" OnServerValidate="newPasswordChecker_ServerValidate" Text="*"></asp:CustomValidator><br />
+            <asp:Label runat="server" ID="confirmPasswordLbl" Text="Confirm Password (if changing): "></asp:Label><br />
+            <asp:TextBox runat="server" ID="confirmPassword" TextMode="Password"></asp:TextBox>
+            <asp:CompareValidator runat="server" ID="confirmPasswordValidator" ControlToCompare="newPassword" 
+                ControlToValidate="confirmPassword" Text="Password must match!" ></asp:CompareValidator><br />
+        </div>
+        <br />
+        <asp:Label runat="server" ID="lastNameLbl" Text="Last Name:"></asp:Label><br />
+        <asp:TextBox runat="server" ID="lastName" ></asp:TextBox><asp:RequiredFieldValidator runat="server" ID="lastNameValidator" 
+            ControlToValidate="lastName" Text="Last Name Required!" CssClass="validator"></asp:RequiredFieldValidator><br />
+        <asp:Label runat="server" ID="firstNameLbl" Text="First Name:"></asp:Label><br />
+        <asp:TextBox runat="server" ID="firstName" ></asp:TextBox><asp:RequiredFieldValidator runat="server" ID="firstNameValidator" 
+            ControlToValidate="lastName" Text="First Name Required!" CssClass="validator"></asp:RequiredFieldValidator><br />
+        <asp:Label runat="server" ID="addressLbl" Text="Address: "></asp:Label><br />
+        <asp:TextBox runat="server" ID="address" ></asp:TextBox><asp:RequiredFieldValidator runat="server" ID="addressValidator" 
+            ControlToValidate="lastName" Text="Address Required!" CssClass="validator"></asp:RequiredFieldValidator><br />
+        <asp:Label runat="server" ID="phoneNumberLbl" Text="Phone Number:"></asp:Label><br />
+        <asp:TextBox runat="server" ID="phoneNumber" TextMode="Phone"></asp:TextBox>
+        <asp:RegularExpressionValidator runat="server" ID="phoneNumberRegexValidator" ControlToValidate="phoneNumber" 
+            ValidationExpression="\(?\d{3}\)?-? *\d{3}-? *-?\d{4}" Text="Must be 10 digit phone number"></asp:RegularExpressionValidator>
+        <asp:RequiredFieldValidator runat="server" ID="phoneNumberValidator" 
+            ControlToValidate="phoneNumber" Text="Phone Required!" CssClass="validator"></asp:RequiredFieldValidator><br />
+        <asp:Label runat="server" ID="birthDateLbl" Text="Birth Date:"></asp:Label><br />
+        <asp:TextBox runat="server" ID="birthDate" TextMode="Date"></asp:TextBox><br />
+        <br />
+        <asp:Button runat="server" ID="saveUpdates" Text="Update" OnClick="saveUpdates_Click" />
+    </div>
 </asp:Content>
--- SQLConnectionWebApp/Pages/MyAccount.aspx.cs
+++ SQLConnectionWebApp/Pages/MyAccount.aspx.cs
@@ -1,17 +1,139 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
+using System.Web.UI.HtmlControls;
 using System.Web.UI.WebControls;
 
 namespace SQLConnectionWebApp.Models
 {
     public partial class WebForm3 : System.Web.UI.Page
     {
+        user User;
+
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            User = user.getUser();
+            if (User.LoggedIn == false)
+            {
+                loggedOut.Visible = true;
+                HtmlMeta meta = new HtmlMeta();
+                meta.HttpEquiv = "Refresh";
+                meta.Content = "10;url=Login.aspx";
+                this.Page.Controls.Add(meta);
+            }
+            else
+            {
+                loggedIn.Visible = true;
+                Get_User_Data();
+            }
+        }
 
+        private void Get_User_Data()
+        {
+            string ConnString = ConfigurationManager.ConnectionStrings["c432018fa01dillnConnectionString"].ConnectionString;
+            string SqlQuery = "SELECT * FROM Users WHERE username=@username";
+            try
+            {
+                // create connection and command objects
+                SqlConnection cnn = new SqlConnection(ConnString);
+                SqlCommand cmd = new SqlCommand(SqlQuery, cnn);
+                // define parameters and their values
+                cmd.Parameters.Add("@username", SqlDbType.VarChar).Value = User.Username;
+                // open connection, execute SELECT, close connection
+                cnn.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+                reader.Read();
+                object[] userValues = new object[reader.FieldCount];
+                reader.GetValues(userValues);
+
+                // assign values to fields
+                usernamePrintLbl.Text = userValues[0].ToString();
+                lastName.Text = userValues[2].ToString();
+                firstName.Text = userValues[3].ToString();
+                address.Text = userValues[4].ToString();
+                birthDate.Text = userValues[5].ToString();
+                phoneNumber.Text = userValues[6].ToString();
+
+                reader.Close();
+                cmd.Dispose();
+                cnn.Close();
+            }
+            catch (Exception ex)
+            {
+                /*lblErrorMessage.Text = "Invalid Username!";
+                lblErrorMessage.Visible = true;*/
+            }
+        }
+
+        protected void saveUpdates_Click(object sender, EventArgs e)
+        {
+            string ConnString = ConfigurationManager.ConnectionStrings["c432018fa01dillnConnectionString"].ConnectionString;
+            string SqlQuery = "Update Users SET password=@password, lastName = @lastName, " +
+                "firstName = @firstName, address = @address, birthDate = @birthDate, " +
+                "phone = @phone WHERE username = @username";
+            try
+            {
+                // create connection and command objects
+                SqlConnection cnn = new SqlConnection(ConnString);
+                SqlCommand cmd = new SqlCommand(SqlQuery, cnn);
+                // define parameters and their values
+                cmd.Parameters.Add("@username", SqlDbType.VarChar).Value = User.Username;
+                if (newPassword.Text == "" && confirmPassword.Text == "")
+                {
+                    cmd.Parameters.Add("@password", SqlDbType.VarChar).Value = oldPassword.Text;
+                } else
+                {
+                    cmd.Parameters.Add("@password", SqlDbType.VarChar).Value = newPassword.Text;
+                }
+                cmd.Parameters.Add("@lastName", SqlDbType.VarChar).Value = lastName.Text;
+                cmd.Parameters.Add("@firstName", SqlDbType.VarChar).Value = firstName.Text;
+                cmd.Parameters.Add("@address", SqlDbType.VarChar).Value = address.Text;
+                cmd.Parameters.Add("@birthDate", SqlDbType.VarChar).Value = DateTime.Parse(birthDate.Text).Date.ToString("yyyy'-'MM'-'dd");
+                cmd.Parameters.Add("@phone", SqlDbType.VarChar).Value = phoneNumber.Text;
+
+                // open connection, execute SELECT, close connection
+                cnn.Open();
+
+                cmd.ExecuteNonQuery();
+                
+                cnn.Close();
+                Response.Redirect("~/Pages/MyAccount.aspx");
+            }
+            catch (Exception ex)
+            {
+                /*lblErrorMessage.Text = "Invalid Username!";
+                lblErrorMessage.Visible = true;*/
+            }
+        }
+
+        protected void newPasswordChecker_ServerValidate(object source, ServerValidateEventArgs args)
+        {
+            if (args.Value.Length <= 8) {
+                newPasswordChecker.Text = "Password must be at least 8 characters";
+                args.IsValid = false;
+            } else if (!args.Value.Any(char.IsUpper))
+            {
+                newPasswordChecker.Text = "Password contain a capital letter";
+                args.IsValid = false;
+            } else if (!args.Value.Any(char.IsNumber))
+            {
+                newPasswordChecker.Text = "Password must contain a number";
+                args.IsValid = false;
+            } else if (!(args.Value.Any(char.IsPunctuation) || args.Value.Any(char.IsSymbol)))
+            {
+                newPasswordChecker.Text = "Password must contain a special character";
+                args.IsValid = false;
+            }
+            else
+            {
+                args.IsValid = true;
+            }
         }
     }
 }
